ae404780e5c920d6262162a66640843f5c6abc2b
Kapena Lumumba <217016375@mycput.ac.za>
2024-04-12 16:05:41 +0200

    apollo

--- src/helpers/apollo.ts
+++ src/helpers/apollo.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { STR_TOKEN } from "./common";
 
 const apollo = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
+  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT + "/graphql",
   cache: new InMemoryCache(),
   headers: {
     authorization: localStorage.getItem(STR_TOKEN) || "",
